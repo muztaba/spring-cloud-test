75a1e1bf6f96e2b6bd55a293652a9db5f1019d4a
muztaba <hasanat.muztaba@gmail.com>
2022-08-11 01:32:04 -0500

    Add resilience4j

--- orderservice/src/main/java/com/seal/orderservice/service/BookService.java
+++ orderservice/src/main/java/com/seal/orderservice/service/BookService.java
@@ -1,6 +1,7 @@
 package com.seal.orderservice.service;
 
 import com.seal.orderservice.dto.Book;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import java.util.List;
 @FeignClient("book-service")
 public interface BookService {
 
+    @CircuitBreaker(name = "bookService")
     @RequestMapping(method = RequestMethod.GET, value = "/books", consumes = "application/json")
     List<Book> getAllBooks();
 
